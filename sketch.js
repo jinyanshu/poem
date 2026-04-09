4829d90a7f86f1e9662041ad201c4f0e09c1b693
amysuer <amysuer@iu.edu>
2026-04-09 11:24:35 -0400

    edit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 //variables
-var one, one1;
+var one;
 
 function preload() {
   one = loadImage("images/one.gif");
-  one1 = createImage("images/one.gif");
+  one = createImage("images/one.gif");
 }
 
 function setup() {
