c66a2f84755b71393a600c6efd5aceee4767f9ba
amysuer <amysuer@iu.edu>
2026-04-13 11:03:40 -0400

    edits

---
+++ five_sketch.js
@@ -0,0 +1,48 @@
+//variables
+var five;
+
+function preload() {
+  five = loadImage("images/5gif.gif");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background("white");
+}
+
+function draw() {
+  //background
+    // Create a p5 element for the background image and add width and height as methods. This allows it to always cover the canvas 
+  let imgRatio = five.width / five.height;
+    // here you define the aspect ratio of the canvas
+  let canvasRatio = width / height;
+    // here you are creating variables that can be used to adjust the background image to the canvas ratio
+  let drawW, drawH, drawX, drawY;
+
+    if (canvasRatio > imgRatio) {
+    // if the canvas is wider than image ratio then fit to width
+    drawW = width;
+    drawH = width / imgRatio;
+  } else {
+    // if the canvas is taller than image ratio — fit to height
+    drawH = height;
+    drawW = height * imgRatio;
+
+    //  image(var, 0, 0, windowWidth, windowHeight);
+}
+
+// centers the image to the canvas
+  drawX = (width - drawW) / 2;
+  drawY = (height - drawH) / 2;
+  
+  //draws the image to align with the center and be as tall as the width and height of the canvas
+  image(five, drawX, drawY, drawW, drawH);
+  
+  // poem appears here
+  
+}
+
+//this resizes the canvas to the width and height of the browser window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
